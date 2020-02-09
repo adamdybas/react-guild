a10824370f44b563cb74b01c646d0f2005039a12
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2020-02-09 17:59:58 +0100

    Move base client config to separate file

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,2 +1,8 @@
 import axios from 'axios';
 
+export default axios.create({
+  baseURL: 'https://api.unsplash.com/',
+  headers: {
+    Authorization: 'Client-ID 967db0e580f206768b2a58782004f5c2052d94be00d661ba7d451aaa66bb7329'
+  }
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash.js';
 import SearchBar from './SearchBar.js';
 
 class App extends React.Component {
   state = { resultData: [] };
 
   onSearchSubmit = async (term) => {
-    const resp = await axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term },
-      headers: {
-        Authorization: 'Client-ID 967db0e580f206768b2a58782004f5c2052d94be00d661ba7d451aaa66bb7329'
-      }
+    const resp = await unsplash.get('search/photos', {
+      params: { query: term }
     });
 
     this.setState({resultData: resp.data.results});
@@ -20,6 +17,7 @@ class App extends React.Component {
     return(
       <div className='ui container' style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found {this.state.resultData.length} images
       </div>
     );
   }
