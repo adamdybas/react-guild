eb9768608c943a23c6cb5024930f30c56509fc6b
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2020-02-09 17:41:06 +0100

    download images from axios; async-await

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar.js';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  state = { resultData: [] };
+
+  async onSearchSubmit(term) {
+    const resp = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 967db0e580f206768b2a58782004f5c2052d94be00d661ba7d451aaa66bb7329'
+      }
+    });
+
+    console.log(resp.data.results);
   }
 
   render() {
