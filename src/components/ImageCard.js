1eae9cc862c00a09aa006cc1bce7dbedf3f8c33f
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2020-02-10 22:36:44 +0100

    Add refs; load image event listener; dynamic grid row end

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.imageRef = React.createRef();
+    this.state = { spansCount: 1 };
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    this.setState({spansCount: Math.ceil(this.imageRef.current.clientHeight/150)});
+  }
+
   render() {
     const { alt_description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style={{gridRowEnd: `span ${this.state.spansCount}`}}>
         <img
+          ref={this.imageRef}
           src={urls.regular}
           alt={alt_description}
-          style={{gridRowEnd: 'span 2'}}
         />
       </div>
     )
