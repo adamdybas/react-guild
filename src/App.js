2726db5e0cf06b6d6b6b9e5a9e91b43b76aaa73b
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2020-02-18 23:48:50 +0100

    Basic use state: button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [ activated, setActivated ] = useState(false);
+
+  const onClick = () => {
+    setActivated(!activated);
+  };
+
+  const buttonText = activated ? "ON" : "OFF";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={onClick}>{ buttonText }</button>
   );
 }
 
