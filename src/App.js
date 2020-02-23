793586af00364648fa0bdf0cee5f4f7158cd2bd4
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2020-02-23 20:28:10 +0100

    React context passing state: provider => consumer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const NameContext = createContext();
+
+class Child extends React.Component {
+  render() {
+    return (
+      <section id='child'>
+        <GrandChild />
+      </section>
+    );
+  }
+}
+
+class GrandChild extends React.Component {
+  render() {
+    return (
+      <div id='grand-child'>
+        <Button />
+      </div>
+    );
+  }
+}
+
+
+class Button extends React.Component {
+  render() {
+    return (
+      <NameContext.Consumer>
+        {
+          name => {
+            return (
+              <button>{name}</button>
+            );
+          }
+        }
+      </NameContext.Consumer>
+    );
+  }
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'Testowy' };
+  }
+
+  render() {
+    return (
+      <NameContext.Provider value={this.state.name}>
+        <Child />
+      </NameContext.Provider>
+    );
+  }
 }
 
 export default App;
