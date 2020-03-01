4875375e05fbcee697d2e25cda85162fe88d3dba
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2020-03-01 19:53:10 +0100

    Shortened version from tutorial

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-  return(
-    async(dispatch, getState) => {
-      const results = await jsonPlaceholder.get('/posts');
+export const fetchPosts = () => async dispatch => {
+  const results = await jsonPlaceholder.get('/posts');
 
-      return dispatch({
-        type: "FETCH_POSTS",
-        payload: results.data
-      })
-    }
-  )
-};
+  dispatch({ type: "FETCH_POSTS", payload: results.data })
+}
